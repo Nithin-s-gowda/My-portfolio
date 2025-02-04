25727ee810e4d4115e3bc60460bed7e8fae35e35
Nithin-s-gowda <nithingowdasn12@gmail.com>
2025-02-04 18:27:34 +0530

    second commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,8 +37,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        'service_v42ifum',
-        'template_v2jkr5i',
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "Nithin S",
@@ -46,7 +46,7 @@ const Contact = () => {
           to_email: "nithingowdasn12@gmail.com",
           message: form.message,
         },
-        'P7XEGiVKQJ4Te5Fo9'
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
